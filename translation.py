1fa6124eca62fae5bc8255b1b6deafa7587a0581
notreallyshikhar <63592764+notreallyshikhar@users.noreply.github.com>
2021-04-30 00:52:48 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,20 +1,12 @@
 class Translation(object):
     START_TEXT = """Hi!
-please read the TnC before proceeding: https://t.me/SpEcHlDe/889
-Thank you for using me 😬
+    
 Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
 
 /start at any stage to re-enter your details"""
-    AFTER_RECVD_CODE_TEXT = """I see!
-now please send the Telegram code that you received from Telegram!
-
-this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pink Button
-
-/start at any stage to re-enter your details"""
-    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
+    AFTER_RECVD_CODE_TEXT = """Please send the Telegram code that you received from Telegram!"""
+    BEFORE_SUCC_LOGIN = "Recieved code. Scrapping web page ..."
+    ERRED_PAGE = "Something went wrong. failed to get app id. \n\n@MysticOp"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
-    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
+    IN_VALID_CODE_PVDED = "Sorry, but the input does not seem to be a valid Telegram Web-Login code"
+    IN_VALID_PHNO_PVDED = "Sorry, but the input does not seem to be a valid phone number"
