e4d5c8a2e95e63135a132658ad0acc62d223822b
MysticKaBaap <83425390+MysticKaBaap@users.noreply.github.com>
2021-04-30 07:08:34 +0530

    Update helper_steps.py

--- helper_funcs/helper_steps.py
+++ helper_funcs/helper_steps.py
@@ -35,35 +35,12 @@ def parse_to_meaning_ful_text(input_phone_number, in_dict):
     me_t += "<i>Phone Number</i>: "
     me_t += f"<u>{input_phone_number}</u>"
     me_t += "\n"
-    me_t += "\n"
-    me_t += "<i>App Configuration</i>"
-    me_t += "\n"
     me_t += "<b>APP ID</b>: "
     me_t += "<code>{}</code>".format(in_dict["App Configuration"]["app_id"])
     me_t += "\n"
     me_t += "<b>API HASH</b>: "
     me_t += "<code>{}</code>".format(in_dict["App Configuration"]["api_hash"])
     me_t += "\n"
-    me_t += "\n"
-    me_t += "<i>Available MTProto Servers</i>"
-    me_t += "\n"
-    me_t += "<b>Production Configuration</b>: "
-    me_t += "<code>{}</code> <u>{}</u>".format(
-        in_dict["Available MTProto Servers"]["production_configuration"]["IP"],
-        in_dict["Available MTProto Servers"]["production_configuration"]["DC"]
-    )
-    me_t += "\n"
-    me_t += "<b>Test Configuration</b>: "
-    me_t += "<code>{}</code> <u>{}</u>".format(
-        in_dict["Available MTProto Servers"]["test_configuration"]["IP"],
-        in_dict["Available MTProto Servers"]["test_configuration"]["DC"]
-    )
-    me_t += "\n"
-    me_t += "\n"
-    me_t += "<i>Disclaimer</i>: "
-    me_t += "<u>{}</u>".format(
-        in_dict["Disclaimer"]
-    )
     return me_t
 
 
